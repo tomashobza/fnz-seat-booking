ee45199d739f7d45fc0cc0cb02a0fb2344fe3c37
Tomáš Hobza <thomashobza@ids.company>
2023-08-20 11:36:47 +0200

    bruh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,7 +40,7 @@
 					class="text-2xl md:text-4xl font-light leading-[3rem] flex flex-col items-center gap-2"
 				>
 					<div class="text-center">Your seat for {day}:</div>
-					{#if d_num < 6 || d_num > 0}
+					{#if d_num < 6 && d_num > 0}
 						<div
 							class="bg-blue-500 font-bold bg-opacity-30 rounded-lg px-3 py-2 text-3xl md:text-5xl mb-10"
 						>
